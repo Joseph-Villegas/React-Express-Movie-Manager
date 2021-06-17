8e3f254a677260fdedb12089fa164e5c92a1b3a0
Joseph-Villegas <jovillegas@csumb.edu>
2021-06-16 17:35:33 -0700

    added endpoint to get all new releases

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const { tmdb_title_search, tmdb_id_advanced_search } = require("./util/tmdb");
+const db = require("./util/db");
 
 /**
  * Searches for a film(s) given a title OR a TMDb ID through the TMDb API
@@ -20,6 +21,17 @@ const { tmdb_title_search, tmdb_id_advanced_search } = require("./util/tmdb");
         const film = await tmdb_id_advanced_search(req.query.id);
         return res.json({ success: true, films: film, message: "Film ID query successfully processed" });
     }
+});
+
+/**
+ * Retrieves all new dvd releases grouped by week
+ */
+ router.get('/new-releases', async (req, res) => {
+    const [data, error] = await db.newReleases.all();
+    if (error) return res.json({ success: false, message: "ERR: Could not retrieve new releases." });
+
+    const releases = data.reduce((hash, obj) => ({...hash, [obj.RELEASE_WEEK]:( hash[obj.RELEASE_WEEK] || [] ).concat(obj)}), {});
+    return res.json({ success: true, message: "New releases retrieved.", releases });
 });
 
 module.exports = router;
